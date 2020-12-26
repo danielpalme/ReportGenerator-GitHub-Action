a7029a3237beb5b7a07f86ff7ffcc501005904a9
danielpalme <daniel.palme@gmx.de>
2020-12-26 12:25:12 +0100

    Release 4.8.3

--- src/reportgenerator.ts
+++ src/reportgenerator.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as fs from 'fs';
 
-const VERSION = '4.8.2';
+const VERSION = '4.8.3';
 
 async function run() {
   try {
