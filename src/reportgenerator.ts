71e61f240f46e4c42f46d2f90c7254202978b05e
danielpalme <daniel.palme@gmx.de>
2022-11-16 21:58:38 +0100

    Release 5.1.12

--- src/reportgenerator.ts
+++ src/reportgenerator.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as fs from 'fs';
 
-const VERSION = '5.1.11';
+const VERSION = '5.1.12';
 
 async function run() {
   try {
