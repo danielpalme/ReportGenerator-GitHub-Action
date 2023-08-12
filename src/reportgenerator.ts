31a65809f47d20322db0bb4d9cb51f2d535f94e0
danielpalme <daniel.palme@gmx.de>
2023-08-12 22:08:48 +0200

    5.1.24

--- src/reportgenerator.ts
+++ src/reportgenerator.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as fs from 'fs';
 
-const VERSION = '5.1.23';
+const VERSION = '5.1.24';
 
 async function run() {
   try {
